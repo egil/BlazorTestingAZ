2c2961d03c5fb23beccc12e61b47694d19951b86
Egil Hansen <egil@assimilated.dk>
2023-10-18 20:06:07 +0100

    snapshot

---
+++ NdcDemo.E2ETests/SnapshotTesting/CounterPageSnapshotTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VerifyTests;
+using VerifyTests.Playwright;
+
+namespace NdcDemo.E2ETests.SnapshotTesting;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+internal class CounterPageTest : BlazorPageTest<Program>
+{
+    [Test]
+    public async Task Counter_page()
+    {
+        // Arrange
+        await Page.GotoPreRenderedAsync("counter");
+
+        await Verify(Page)
+            .PageScreenshotOptions(
+                new()
+                {
+                    Quality = 50,
+                    Type = ScreenshotType.Jpeg
+                });
+    }
+
+}
---
+++ NdcDemo.E2ETests/SnapshotTesting/VerifyInit.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace NdcDemo.E2ETests.SnapshotTesting;
+
+public static class VerifyInit
+{
+    [ModuleInitializer]
+    public static void InitPlaywright()
+        => VerifyPlaywright.Initialize();
+}
+
